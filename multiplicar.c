dad29399e47fcb026b06937162c69600628ff7e0
JaoViana327 <joaopviana719@gmail.com>
2025-11-25 19:57:38 -0300

    Update multiplicar.c

--- multiplicar.c
+++ multiplicar.c
@@ -1,4 +1,17 @@
+#include <stdio.h>
 
-int multiplicar(int a, int b) {
-    return a * b;
+int multiplicar(int numero1, int numero2, int contador1, int contador2, int produto){
+     contador1 = 0;
+    contador2 = 0;
+    produto = 0;
+
+    while (contador1 < numero1){
+        contador2 = 0;
+        while (contador2 < numero2){
+            produto++; // produto = produto + 1;
+            contador2++;   
+        }
+        contador1++;    
+    }
+    return produto;
 }
